720ce00cad8a075adda145d3591b5c09981af9df
jdlrobson <jdlrobson@gmail.com>
2018-03-22 16:59:14 -0700

    Switch to span - caption element for tables

--- src/components/Thumbnail/index.js
+++ src/components/Thumbnail/index.js
@@ -4,9 +4,10 @@ import './index.css'
 const Thumbnail = ( { src, caption, width, height, source, sourceName = 'source' } ) => (
   <div className="thumb">
     <img src={src} alt={caption} className="thumb__image" width={width} height={height} />
-    <caption className="thumb__caption">{caption}
+    <span className="thumb__caption">
+      {caption}
       <small className="thumb__source"><a href={source}>{sourceName}</a></small>
-    </caption>
+    </span>
   </div>
 )
 

ecabb475eaaf34c03f44aad50503212584e82dae
jdlrobson <jdlrobson@gmail.com>
2018-03-22 18:00:16 -0700

    Thumbnails can be floated

--- src/components/Thumbnail/index.js
+++ src/components/Thumbnail/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 import './index.css'
 import { BG_COLOR } from './../../const'
 
-const Thumbnail = ( { src, caption, width, height, source, sourceName = 'source' } ) => (
-  <div className="thumb" style={ {background: BG_COLOR} }>
-    <img src={src} alt={caption} className="thumb__image" width={width} height={height} />
+const Thumbnail = ( { src, caption, width, height, source, float = 'none', sourceName = 'source' } ) => (
+  <div className={"thumb thumb--float-" + float }
+    style={ {background: BG_COLOR, float: float } }>
+    <img src={src} alt={caption} className="thumb__image"
+      width={width} height={height} />
     <span className="thumb__caption">
       {caption}
-      <small className="thumb__source"><a href={source}>{sourceName}</a></small>
+      { source && (
+        <small className="thumb__source"><a href={source}>{sourceName}</a></small>
+      ) }
     </span>
   </div>
 )

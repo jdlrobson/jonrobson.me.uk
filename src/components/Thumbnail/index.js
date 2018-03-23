f6ebfd707b47f7b3cd489490ffead24993bd7f1a
jdlrobson <jdlrobson@gmail.com>
2018-03-22 19:52:12 -0700

    Thumbnail component tweaks

--- src/components/Thumbnail/index.js
+++ src/components/Thumbnail/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import './index.css'
 import { BG_COLOR } from './../../const'
 
-const Thumbnail = ( { src, caption, width, height, source, float = 'none', sourceName = 'source' } ) => (
-  <div className={"thumb thumb--float-" + float }
-    style={ {background: BG_COLOR, float: float } }>
+const Thumbnail = ( { src, caption, width, height, source, small = false,
+  float = 'none', sourceName = 'source'
+} ) => (
+  <div className={"thumb thumb--float-" + float + ' thumb--' + ( small ? 'small' : 'reg' ) }
+    style={ { float: float } }>
     <img src={src} alt={caption} className="thumb__image"
       width={width} height={height} />
     <span className="thumb__caption">

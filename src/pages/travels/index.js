5f386f23e28c795e1328eec0ced93b2798d330ca
jdlrobson <jdlrobson@gmail.com>
2018-03-22 18:27:12 -0700

    Add Travel stub

---
+++ src/pages/travels/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Thumbnail from './../../components/Thumbnail'
+import map from './map.png'
+
+export default () => (
+  <div>
+    <h1>Travel</h1>
+    <p>Travelling has always been a big part of my life. I should write about it more.</p>
+    <Thumbnail src={map}
+      caption="Like many travelers, I maintain a travel map"
+      source="https://www.tripadvisor.com/TravelMap-a_uid.2D3DFB1F097C7157C8B2F126998DDAF5"
+      sourceName="tripadvisor" />
+  </div>
+);

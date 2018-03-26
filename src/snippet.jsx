5c3270df5f09ff9471f3250af127efef2634d498
jdlrobson <jdlrobson@gmail.com>
2018-03-25 17:49:04 -0700

    Allow combining of snippets

--- src/snippet.jsx
+++ src/snippet.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {markdown} from 'markdown'
 
-export default function ( text ) {
+export default function () {
+  const text = Array.from( arguments ).join( '\n' );
   return <div dangerouslySetInnerHTML={{__html: markdown.toHTML(text)}}></div>
 };

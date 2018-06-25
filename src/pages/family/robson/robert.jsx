7cde353c161aeb453a59240eea419fea8f1561e1
jdlrobson <jdlrobson@gmail.com>
2018-06-24 18:05:57 -0700

    Family tree: Add William

--- src/pages/family/robson/robert.jsx
+++ src/pages/family/robson/robert.jsx
@@ -7,6 +7,7 @@ export default () => (
   <div>
     <h1>The Robson's</h1>
     <h2>Robert Robson</h2>
+    <p>Son of <a href="/family/robson">John Robson</a> and <strong>Jane Watson</strong>.</p>
     {snippet(robert)}
   </div>
 );

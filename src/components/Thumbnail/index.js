59808369f883d6fa604c9df5e3f7c8233eb871ff
jdlrobson <jdlrobson@gmail.com>
2018-06-24 18:06:12 -0700

    Thumbnail should support large mode

--- src/components/Thumbnail/index.js
+++ src/components/Thumbnail/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './index.css'
 import { BG_COLOR } from './../../const'
 
-const Thumbnail = ( { src, caption, width, height, source, small = false,
+const Thumbnail = ( { src, caption, width, height, source, large = false, small = false,
   float = 'none', sourceName = 'source'
 } ) => (
-  <div className={"thumb thumb--float-" + float + ' thumb--' + ( small ? 'small' : 'reg' ) }>
+  <div className={"thumb thumb--float-" + float + ' thumb--' + ( small ? 'small' : ( large ? 'lrg' : 'reg' ) ) }>
     <img src={src} alt={caption} className="thumb__image"
       width={width} height={height} />
     <span className="thumb__caption">

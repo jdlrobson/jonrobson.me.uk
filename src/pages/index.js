ef02134f301c32934048a724274dc1d3cc254962
jdlrobson <jdlrobson@gmail.com>
2018-11-23 21:13:47 -0800

    Layout casing (3rd time lucky?)

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 import Thumbnail from './../components/Thumbnail'
 import { ME_IMG_URL } from './../const'
 
-import Layout from "./../components/layout"
+import Layout from "./../components/Layout"
 
 const IndexPage = ( props ) => (
   <Layout {...props}>

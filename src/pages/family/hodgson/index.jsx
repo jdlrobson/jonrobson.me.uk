07296f344f923db1f898822af7f53f4d50608e7a
jdlrobson <jdlrobson@gmail.com>
2018-12-28 13:54:57 -0800

    Amendments to William Robson + the Hodgsons

---
+++ src/pages/family/hodgson/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Layout from "../../../../components/Layout";
+
+export default ( props ) => (
+  <Layout {...props}>
+    <h2>Christopher Hill Hodgson</h2>
+    <em>Thank you to Clive Hodgson for providing all this information.</em>
+    <p>Christopher Hill was born 2nd February 1863</p>
+    <p>He was a grocer and part-time photographer.</p>
+    <p>On 2nd August 1886 he married Jane Anne Robson (born 1862), daughter of <a href="/family/robson/William/">William Robson</a></p>
+    <p>They had four children, Norah (born 15th May 1889), William Robson (born 25th May 1892),  Ena (6th August 1894) and Mary (23rd July 1898).
+    </p>
+    <p>His son William Robson was a grocer, confectionier, assumingly he continued the family business.</p>
+    <p>William Robson married Nora Blackett (19/12/1893-9/4/1973) on 1st January 1919 (happy new year!), </p>
+    <p>Christopher Hill died 26th October 1926.</p>
+    <p>Christopher's daughters Norah and Ena moved to New Zealand where they died 20th May 1974 and 3rd February 1980 respectively.</p>
+  </Layout>
+);
--- src/pages/family/robson/William/index.jsx
+++ src/pages/family/robson/William/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import photo from './photo.jpg'
 import goldenwedding from './goldenwedding2.jpg'
 import grave from './grave.jpg'
+import timber from './Robson-Timber.jpg'
 
 import Thumbnail from './../../../../components/Thumbnail'
 import Layout from "../../../../components/Layout";
@@ -17,13 +18,13 @@ export default ( props ) => (
        caption="Family portrait of William, wife Jane and family. Pictured is grandchild Arthur (son of John Thomas) and some of their children including son-in-law (Christopher Hill Hodgson). The back of the photo states it was taken at 'The Farm'.
        The location is a mystery. Do you know?"
     />
-    <p>They had <strong>eight</strong> children - the first two were born in Blythe, Northumberland: <strong>John Thomas Robson
+    <p>They had <strong>nine</strong> known children - the first two were born in Blythe, Northumberland: <strong>John Thomas Robson
       </strong> (born 1859) and <strong>Margaret Robson</strong> (born 1860); <strong>Jane Ann
-      </strong> (born 1863 - birthplace unknown); and then the latter 5 in Sunderland - <strong>Isabella</strong> Robson (1865), <strong>Barbara
+      </strong> (born 1863-1931); and then the latter 5 in Sunderland - <strong>Isabella</strong> Robson (1865), <strong>Barbara
       </strong>(1867), <strong>Mary Anna</strong> (1869), <strong>Emily
-      </strong> (1872) and <strong>William</strong> (1879).</p>
+      </strong> (1872-1924), William (1877 who died at 20 months old) and <strong>William</strong> (1879).</p>
     <p>John Thomas (known as Jack) married a lady called Dorothy. They had six children - all pictured below - Harry, Dorothy (Dot), Jennie, Winnie, Arthur (also pictured above) and Bertha.</p>
-    <p>Jane Ann, married Christopher Hill <strong>Hodgson</strong> (a part time photographer) and had four children, William Robson Hodgson (WRH), Norah, Ena and Mary. All are pictured in the photo above.</p>
+    <p>Jane Ann, married Christopher Hill <a href="/family/hodgson">Hodgson</a> (a grocer and part time photographer) and had four children, William Robson Hodgson (WRH), Norah, Ena and Mary. All are pictured in the photo above.</p>
     <Thumbnail src={goldenwedding} large={true}
        caption="Family portrait of William and family sent to New Zealand in the 1920's."
     />
@@ -31,11 +32,14 @@ export default ( props ) => (
     <p>Mary Anna (known as Nanna) married Jack Curry and had one known child Jack Curry Junior.</p>
     <p>Their youngest son, William married Amy and had two children, shown but names unknown.</p>
     <p>William Robson worked in the timber business (W. Robson & Co., East Side, South Dock, Sunderland). His two sons continued the business.</p>
+    <Thumbnail src={timber} large={true}
+       caption="The timber business owned by William Robson"
+    />
     <p>In 1886 the Robson family were living at 6 Harold Street, Sunderland (from marriage certificate). The 1871 census still has them at this address. They had previously lived at 36 Henry Street (1861 census), sharing it with another family.
 Harold Street lay off South Salem Street and was destroyed in WW2. Harold Square now stands on the location. According to the 1911 census return, the house in Harold Street had 9 rooms (on 3 floors). Plans show it to be terraced. Similar houses exist in adjacent street (2009).</p>
-    <p>He died in 1914 a rich man leaving the very considerable sum of £24,058-6-0. His gross estate was worth (2009 £s) £1,710,000 by rpi or £8,950,000 using average earnings.</p>
+    <p>He died January 12th 1914 a rich man leaving the very considerable sum of £24,058-6-0. His gross estate was worth (2009 £s) £1,710,000 by rpi or £8,950,000 using average earnings.</p>
     <Thumbnail src={grave} large={true}
-       caption="Gravestone of William Robson."
+       caption="Gravestone of William Robson. It reads In loving memory of William Robson died January 12th 1914 aged 79 years and of Jane Robson his wife died February 3rd 1916 aged 84 years. Emily Robson daughter of above died October 25th 1924 died 32 years and William son of above died April 28th 1877 aged 1 year & 8 months."
     />
   </Layout>
 );

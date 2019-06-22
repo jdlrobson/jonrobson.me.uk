300a3e300f4ce5db820de1d74bc705a22cac76b9
jdlrobson <jdlrobson@gmail.com>
2019-06-22 14:07:25 +0100

    Gartside -> wiki

--- src/pages/family/gartside/index.jsx
+++ src/pages/family/gartside/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Layout from './../../../components/Layout'
+import Layout from './../../../components/Layout';
+import marked from '../../../marked';
+import snippet from 'raw-loader!../../../../jonrobson.me.uk.wiki/gartside.md';
 
 export default ( props ) => (
   <Layout {...props}>
-    <h2>The Gartside family tree</h2>
-    <p><a href="http://jdlrobson.com/familytree/Henry%20Gartside%201526.html">The Gartside family
-    tree</a> can be traced back to Henry Gartside in 1526.</p>
+    <div dangerouslySetInnerHTML={marked(snippet)} />
   </Layout>
 );

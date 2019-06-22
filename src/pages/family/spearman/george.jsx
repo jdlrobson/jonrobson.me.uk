3df30040c8ef0b882da7289adb8de9296dc3f8e1
jdlrobson <jdlrobson@gmail.com>
2019-06-22 14:38:01 +0100

    All the things to the wiki!!
    Now all the family tree content lives in the wiki.
    The next stage willl be to work around the pages from the wiki contents..
    
    First let's check this exercise worked correctly
    
    pick a53a48c oxxfi

--- src/pages/family/spearman/george.jsx
+++ src/pages/family/spearman/george.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import Layout from "../../../components/Layout"
+import snippet from 'raw-loader!../../../../jonrobson.me.uk.wiki/spearman_george.md';
+import marked from '../../../marked';
 
 export default ( props ) => (
   <Layout {...props}>
     <h1>The Spearman's</h1>
-    <h2>George Spearman</h2>
-    <p>George was born in 1760 to <strong>Gilbert Illegy SPEARMAN</strong> and <strong>Anna Clark</strong></p>
-    <p>He married <strong>Hannah Woodhouse</strong> in 12th June 1763.</p>
-    <p>His children were <strong>Isaac Woodhouse SPEARMAN</strong>, <a href="/family/spearman/woodhouse">Woodhouse Spearman</a> and <strong>
-        Hannah Spearman</strong></p>
-    <p>George died in 1828</p>
+    <div dangerouslySetInnerHTML={marked(snippet)} />
   </Layout>
 );
--- src/pages/family/spearman/woodhouse.jsx
+++ src/pages/family/spearman/woodhouse.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import Layout from "../../../components/Layout"
+import Layout from "../../../components/Layout";
+import snippet from 'raw-loader!../../../../jonrobson.me.uk.wiki/spearman_woodhouse.md';
+import marked from '../../../marked';
 
 export default ( props ) => (
   <Layout {...props}>
     <h1>The Spearman's</h1>
-    <h2>Woodhouse Spearman</h2>
-    <p>Woodhouse was the daughter of <strong>Hannah Woodhouse</strong> and <a href="/family/spearman/george">George Spearman</a>. She was born in Newcastle in July 1787 (12.05pm - but I have lost the date!).</p>
-    <p>Woodhouse married <a href="/family/oxx/john">John Oxx</a> at All Saints, Newcastle Upon Tyne, Northumberland, England with whom she had nine children.</p>
-    <p>She died 5.30pm on 4th April 1826 aged 38 and was buried at St. Edmunds Burying Ground, Gateshead, letter F No. 6.
-    I speculate that she died during the childbirth of her ninth son.</p>
+    <div dangerouslySetInnerHTML={marked(snippet)} />
   </Layout>
 );

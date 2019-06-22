26604c57cb6d508b5a307fe558a12202114cea25
jdlrobson <jdlrobson@gmail.com>
2019-06-22 14:05:29 +0100

    Hunter -> wiki

--- src/pages/family/hunter/index.jsx
+++ src/pages/family/hunter/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Layout from './../../../components/Layout'
+import marked from '../../../marked';
+import snippet from 'raw-loader!../../../../jonrobson.me.uk.wiki/hunter.md';
 
 export default ( props ) => (
   <Layout {...props}>
-    <h2>The Hunter family tree</h2>
-    <p><a href="https://jdlrobson.com/familytree/Frederick%20Hunter.html">The Hunter family
-    tree</a> is traced back only 3 generations.</p>
+    <div dangerouslySetInnerHTML={marked(snippet)} />
   </Layout>
 );
